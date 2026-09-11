f2518a471153a30f46d83d2c6750988921ab09c7
Michael T <nguyentuan25699@gmail.com>
2026-09-11 11:14:52 +0700

    add products CRUD (GET list/filter/by id, POST) - nest; switch to node-modules linker

--- nest-api/src/products/products.controller.ts
+++ nest-api/src/products/products.controller.ts
@@ -1,45 +1,55 @@
-import { BadRequestException, Body, Controller, Get, NotFoundException, Param, Post, Query } from "@nestjs/common";
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-let items = [
+const items = [
   {
     id: 1,
-    name: "Apple",
+    name: 'Apple',
     price: 100000,
-    category: "fruit"
+    category: 'fruit',
   },
   {
     id: 2,
-    name: "Strawberry",
+    name: 'Strawberry',
     price: 500000,
-    category: "fruit"
-  }, {
+    category: 'fruit',
+  },
+  {
     id: 3,
-    name: "Grape",
+    name: 'Grape',
     price: 300000,
-    category: "fruit"
+    category: 'fruit',
   },
   {
     id: 4,
-    name: "Apple Juice",
+    name: 'Apple Juice',
     price: 50000,
-    category: "juice"
+    category: 'juice',
   },
   {
     id: 5,
-    name: "Grape Juice",
+    name: 'Grape Juice',
     price: 100000,
-    category: "juice"
+    category: 'juice',
   },
-]
+];
 
 @Controller('products')
 export class ProductsController {
   @Get()
   findAll(@Query('category') category?: string) {
-    const itemsFound = items.filter((item) => item.category === category)
+    const itemsFound = items.filter((item) => item.category === category);
 
     if (!category) {
-      return items
+      return items;
     }
 
     return itemsFound;
@@ -47,26 +57,26 @@ export class ProductsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    const itemFound = items.find((item) => String(item.id) === id)
+    const itemFound = items.find((item) => String(item.id) === id);
 
     if (!id || !itemFound) {
       throw new NotFoundException('Product not found');
     }
 
-    return itemFound
+    return itemFound;
   }
 
   @Post()
   create(@Body() body: { name: string; price: number; category: string }) {
     const keys = ['name', 'price', 'category'];
     if (keys.some((key) => !body[key as keyof typeof body])) {
-      throw new BadRequestException("Missing infomation")
+      throw new BadRequestException('Missing infomation');
     }
 
     const newId = Math.max(...items.map((item) => item.id)) + 1;
-    const newItem = { ...body, id: newId }
+    const newItem = { ...body, id: newId };
 
-    items.push(newItem)
+    items.push(newItem);
 
     return newItem;
   }
